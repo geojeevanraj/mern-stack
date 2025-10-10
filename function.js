c691c004cc119c25194824ddf334a740b0d9c958
geojeevanraj <geojeevanraj007@gmail.com>
2025-10-10 13:45:06 +0530

    functions

---
+++ function.js
@@ -0,0 +1,18 @@
+// function sum(a, b) {
+//     return a + b;
+// }
+
+// // console.log(sum(5, 10));
+
+// var a = 5;
+// var b = 1;
+
+// console.log(sum(a, b));
+
+// Arrow function
+
+const multiply = (x, y) => x*y;
+var x = 5;
+var y = 10;
+
+console.log(multiply(x, y));
--- test.js
+++ test.js
@@ -1,1 +1,17 @@
-console.log('Test');
+// console.log('Test');
+
+// let ob1= {age:20};
+// let ob2 = ob1;
+// ob1.age=30;
+// console.log(ob2.age);
+
+// console.log(5>10);
+
+console.log(5>>2);
+
+// console.log(5<<2);
+
+// var a = 5;
+// var b = 10;
+
+// console.log(a^b);
