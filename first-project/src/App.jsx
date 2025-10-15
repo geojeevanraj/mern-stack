2db67cd2791720e4bf4f42aaff0884caed47d1ba
geojeevanraj <geojeevanraj007@gmail.com>
2025-10-15 16:17:05 +0530

    navbar

--- first-project/src/App.jsx
+++ first-project/src/App.jsx
@@ -4,14 +4,16 @@ import { Routes, Route } from 'react-router-dom'
 import Home from './pages/Home'
 import About from './pages/About'
 import Contact from './pages/Contact'
+import Navbar from './components/Navbar'
 
 const App = () => {
   return (
     <>
+      <Navbar />
       <Routes>
-        <Route path='/' Component={Home}></Route>
-        <Route path='/about' Component={About}></Route>
-        <Route path='/contact' Component={Contact}></Route>
+        <Route path='/' Component={Home}>Home</Route>
+        <Route path='/about' Component={About}>About</Route>
+        <Route path='/contact' Component={Contact}>Contact</Route>
       </Routes>
 
       <User name="Geo" department="AIML"
