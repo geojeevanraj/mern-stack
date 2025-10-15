ffe8065537c677d3bd8fd866ebcf470b07b91518
geojeevanraj <geojeevanraj007@gmail.com>
2025-10-15 14:33:33 +0530

    routes

--- first-project/src/App.jsx
+++ first-project/src/App.jsx
@@ -1,11 +1,22 @@
 import React from 'react'
 import User from './components/User'
+import { Routes, Route } from 'react-router-dom'
+import Home from './pages/Home'
+import About from './pages/About'
+import Contact from './pages/Contact'
 
 const App = () => {
   return (
     <>
+      <Routes>
+        <Route path='/' Component={Home}></Route>
+        <Route path='/about' Component={About}></Route>
+        <Route path='/contact' Component={Contact}></Route>
+      </Routes>
+
       <User name="Geo" department="AIML"
       skills = {["HTML", "CSS", "JS"]} />
+
     </>
   )
 }
