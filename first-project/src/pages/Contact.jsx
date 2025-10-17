8f79cb92617c99d9ae1fc918411315376156cdaf
geojeevanraj <geojeevanraj007@gmail.com>
2025-10-17 23:11:38 +0530

    z

--- first-project/src/pages/Contact.jsx
+++ first-project/src/pages/Contact.jsx
@@ -6,20 +6,32 @@ const Contact = () => {
     email: "",
     message: ""
   });
+  const handleChange = (e) => {
+    const {name, value} = e.target;
+    setForm(prevForm => ({
+      ...prevForm,
+      [name]: value
+    }));
+  }
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    console.log(form);
+    alert("Form submitted!");
+  }
   return (
     <div>
       <h1>Contact</h1>
-      <form>
+      <form onSubmit={handleSubmit}>
         <label htmlFor="">Name:</label>
-        <input type='text' name='name' value={form.name}
+        <input type='text' name='name' value={form.name} onChange={handleChange}
         placeholder='Enter your name' />
         <br />
         <label htmlFor="">Email:</label>
-        <input type='email' name='email' value={form.email}
+        <input type='email' name='email' value={form.email} onChange={handleChange}
         placeholder='Enter your email' />
         <br />
         <label htmlFor="">Message:</label>
-        <textarea name='message' value={form.message}
+        <textarea name='message' value={form.message} onChange={handleChange}
         placeholder='Enter your message'></textarea>
         <button type='submit'>Send</button>
       </form>
--- first-project/src/pages/Home.jsx
+++ first-project/src/pages/Home.jsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
+
 const Home = () => {
   return (
     <div>
@@ -9,6 +10,7 @@ const Home = () => {
           <Link to = '/state'>useState</Link>
         </li>
       </ul>
+      {/* <User name="Geo" department="AIML" skills={["HTML", "CSS", "JS"]} /> */}
     </div>
   )
 }
