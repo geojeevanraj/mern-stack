2dd5689fde23ba92fdf7dae59b721e1ff5b8a587
geojeevanraj <geojeevanraj007@gmail.com>
2026-01-21 10:10:53 +0530

    .env

--- backend/server.js
+++ backend/server.js
@@ -1,7 +1,9 @@
 const express=require('express');
 const app=express();
 const mongoose=require('mongoose');
+const dotenv=require('dotenv');
 
+dotenv.config();
 app.use(express.json());
 
 mongoose.connect(process.env.MONGO_URL,
